ebd654bca0e6c94a0286a9b1c41b4745318230ed
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-07-17 16:59:55 +0600

    Added 2234 class

---
+++ 2235.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeSolutions
+{
+    internal class _2235
+    {
+        public int Sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
