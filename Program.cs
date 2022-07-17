257d51a51da444786ce18e5c21584d5f80e7a63b
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-07-17 17:03:07 +0600

    2235 AC

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using LeetcodeSolutions;
+
+Console.WriteLine("Solved");
+
+_2235 Obj = new _2235();
+int sum = Obj.Sum(12, 15);
+Console.WriteLine(sum);
+
