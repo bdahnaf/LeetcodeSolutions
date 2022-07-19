2bdbd9d6c9dd99641ee044c3337378749e67854b
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-07-19 23:53:08 +0600

    Added class structure

---
+++ TwoSum1.cs
@@ -0,0 +1,24 @@
+﻿/**
+ * 
+ * Problem URL - https://leetcode.com/problems/two-sum/
+ * Problem Id - 1
+ * Author - Ahnaf Shahriar
+ * Status - Trying
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeSolutions
+{
+    internal class TwoSum1
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+
+        }
+    }
+}
