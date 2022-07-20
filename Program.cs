36e4626613a9ab71a5ad87bc4824a9624a879123
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-07-20 16:31:22 +0600

    removed comment

--- Program.cs
+++ Program.cs
@@ -6,10 +6,4 @@ Console.WriteLine("Solved");
 TwoSum1 Obj = new TwoSum1();
 int[] arg = { 3, 3};
 int[] output = Obj.TwoSum(arg, 6);
-foreach (var item in output)
-{
-    Console.WriteLine(item);
-}
-
-//Console.WriteLine(output[0]);
 
