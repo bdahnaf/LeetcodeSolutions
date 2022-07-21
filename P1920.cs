7f782520ee12fe71565b8d01ac6614e71483b110
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-07-21 15:03:42 +0600

    Accepted

--- P1920.cs
+++ P1920.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/**
+ * 
+ * Problem URL - https://leetcode.com/problems/build-array-from-permutation/
+ * Problem Id - 1920
+ * Author - Ahnaf Shahriar
+ * Status - Accepted
+ * 
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +19,10 @@ namespace LeetcodeSolutions
         public int[] BuildArray(int[] nums)
         {
             int[] ans = new int[nums.Length];
+            for(int i = 0; i < nums.Length; i++)
+            {
+                ans[i] = nums[nums[i]];
+            }
             return ans;
         }
     }
--- Program.cs
+++ Program.cs
@@ -6,5 +6,9 @@ Console.WriteLine("Solved");
 P1920 Obj = new P1920();
 int[] arg = { 0, 2, 1, 5, 3, 4 };
 int[] output = Obj.BuildArray(arg);
+foreach (var item in output)
+{
+    Console.WriteLine(item);
+}
 Console.WriteLine(output[0]);
 
