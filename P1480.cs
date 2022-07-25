17dfbbadc3b9ffbe992e0e33174af5f4e3c6af83
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-07-25 16:22:49 +0600

    Added Class and structure

---
+++ P1480.cs
@@ -0,0 +1,26 @@
+﻿/**
+ * 
+ * Problem URL - https://leetcode.com/problems/running-sum-of-1d-array/
+ * Problem Id - 1480
+ * Author - Ahnaf Shahriar
+ * Status - Trying
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeSolutions
+{
+    internal class P1480
+    {
+        public int[] RunningSum(int[] nums)
+        {
+            int[] ans = new int[] { };
+            return ans;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,8 +3,11 @@ using LeetcodeSolutions;
 
 Console.WriteLine("Solved");
 
-P1281 Obj = new P1281();
-int num = 4421;
-int output = Obj.SubtractProductAndSum(num);
-Console.WriteLine(output);
+P1480 Obj = new P1480();
+int[] nums = {1, 2, 3, 4};
+int[] output = Obj.RunningSum(nums);
+foreach (int i in output)
+{
+    Console.WriteLine(i);
+}
 
