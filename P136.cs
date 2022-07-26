379a9e23c5af1e6a0f46a40bfa46f6b01ab6b243
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-07-26 16:54:01 +0600

    Accepted 136

---
+++ P136.cs
@@ -0,0 +1,39 @@
+﻿/**
+ * 
+ * Problem URL - https://leetcode.com/problems/single-number/
+ * Problem Id - 136
+ * Author - Ahnaf Shahriar
+ * Status - Accepted
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeSolutions
+{
+    internal class P136
+    {
+        public int SingleNumber(int[] nums)
+        {
+            int tempSum = 0;
+            int sumOfNumArray = 0;
+            for(int i = 0; i < nums.Length; i++)
+            {
+                sumOfNumArray = sumOfNumArray + nums[i];
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] == nums[j])
+                    {
+                        tempSum = tempSum + nums[i] * 2;
+                    }
+                }
+            }
+            return sumOfNumArray - tempSum;
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,9 +3,9 @@ using LeetcodeSolutions;
 
 Console.WriteLine("Solved");
 
-P35 Obj = new P35();
-int[] nums = { 1, 3, 5, 6 };
-int target = 0;
-int output = Obj.SearchInsert(nums, target);
+P136 Obj = new P136();
+int[] nums = { 1 };
+
+int output = Obj.SingleNumber(nums);
 Console.WriteLine(output);
 
