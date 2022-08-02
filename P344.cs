a2efd9a4f5705c79c7d0d6985ec089cf49054237
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-08-02 17:29:27 +0600

    Class

---
+++ P344.cs
@@ -0,0 +1,30 @@
+﻿/**
+ * 
+ * Problem URL - https://leetcode.com/problems/reverse-string/
+ * Problem Id - 344
+ * Author - Ahnaf Shahriar
+ * Status - Trying
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeSolutions
+{
+    internal class P344
+    {
+        public void ReverseString(char[] s)
+        {
+            int len = s.Length;
+            Console.WriteLine(len);
+            for (int i = len-1; i >= 0; i--)
+            {
+                Console.WriteLine(s[i]);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,14 +3,13 @@ using LeetcodeSolutions;
 
 Console.WriteLine("Solved");
 
-P1431 Obj = new P1431();
-int[] candies = { 4, 2, 1, 1, 2 };
-int extraCandies = 1;
-IList<bool> output = new List<bool>();
-output = Obj.KidsWithCandies(candies, extraCandies);
-foreach(object obj in output)
-{
-    Console.WriteLine(obj);
-}
+P344 Obj = new P344();
+char[] s = { 'h', 'e', 'l', 'l', 'o' };
+
+Obj.ReverseString(s);
+//foreach(object obj in output)
+//{
+//    Console.WriteLine(obj);
+//}
 //Console.WriteLine(output);
 
