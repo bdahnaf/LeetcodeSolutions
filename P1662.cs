923c0b6537faf5d3632fe3b07fa774026057cd43
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-08-03 15:11:43 +0600

    1662 Accepted

---
+++ P1662.cs
@@ -0,0 +1,40 @@
+﻿/**
+ * 
+ * Problem URL - https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/
+ * Problem Id - 1662
+ * Author - Ahnaf Shahriar
+ * Status - Trying
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeSolutions
+{
+    internal class P1662
+    {
+        public bool ArrayStringsAreEqual(string[] word1, string[] word2)
+        {
+            bool ans = false;
+            string firstString = "";
+            string secondString = "";
+            foreach(var word in word1)
+            {
+                firstString += word;
+            }
+            foreach (var word in word2)
+            {
+                secondString += word;
+            }
+            if(firstString == secondString)
+            {
+                ans = true;
+            }
+            return ans;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,15 +3,10 @@ using LeetcodeSolutions;
 
 Console.WriteLine("Solved");
 
-P1351 Obj = new P1351();
-//int[,] grid = { { 4, 3, 2, -1 }, { 3, 2, 1, -1 }, { 1, 1, -1, -2 }, { -1, -1, -2, -3 } };
-int[,] grid = { { 3, 2}, { 1, 0 } };
-int[][] grid2 = new int[2][];
+P1662 Obj = new P1662();
+string[] word1 = { "a", "cb" };
+string[] word2 = { "ab", "c" };
 
-int output = Obj.CountNegatives(grid2);
-//foreach(object obj in output)
-//{
-//    Console.WriteLine(obj);
-//}
+bool output = Obj.ArrayStringsAreEqual(word1, word2);
 Console.WriteLine(output);
 
